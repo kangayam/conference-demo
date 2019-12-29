81f63872798da23960c4a0a9328607d04203fdce
Kangayam <bkaru1@gmail.com>
2019-12-28 22:52:12 -0600

    test

---
+++ src/main/java/com/conference/conferencedemo/config/PersistenceConfiguration.java
@@ -0,0 +1,24 @@
+package com.conference.conferencedemo.config;
+
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+// This class will override application.properties file. You build your own configuration.
+@Configuration
+public class PersistenceConfiguration {
+    @Bean
+
+    public DataSource dataSource(){
+        DataSourceBuilder builder = DataSourceBuilder.create();
+        builder.url("jdbc:postgresql://localhost:5432/conference_app");
+        builder.username("postgres");
+        builder.password("Welcome");
+        System.out.println("My custom datasource from persistanceConfiguration class");
+        return builder.build();
+
+    }
+}
---
+++ src/main/java/com/conference/conferencedemo/models/Speaker.java
@@ -0,0 +1,99 @@
+package com.conference.conferencedemo.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.hibernate.annotations.Type;
+
+import javax.persistence.*;
+import java.util.List;
+
+
+
+@Entity(name = "speakers")
+@JsonIgnoreProperties({"hibernateLazyinitialzer", "handler"})
+public class Speaker {
+    public Long getSpeaker_id() {
+        return Speaker_id;
+    }
+
+    public void setSpeaker_id(Long speaker_id) {
+        Speaker_id = speaker_id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getSpeaker_bio() {
+        return speaker_bio;
+    }
+
+    public void setSpeaker_bio(String speaker_bio) {
+        this.speaker_bio = speaker_bio;
+    }
+
+    public List<Session> getSessions() {
+        return sessions;
+    }
+
+    public void setSessions(List<Session> sessions) {
+        this.sessions = sessions;
+    }
+
+    public byte[] getSpeaker_photo() {
+        return speaker_photo;
+    }
+
+    public void setSpeaker_photo(byte[] speaker_photo) {
+        this.speaker_photo = speaker_photo;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Speaker_id;
+    private String first_name;
+    private String last_name;
+    private String title;
+    private String company;
+    private String speaker_bio;
+    private byte[] speaker_photo;
+    @Lob
+    @Type(type="org.hibernate.type.binarytype")
+    @ManyToMany(mappedBy = "speakers")
+    @JsonIgnore
+    public List<Session> sessions;
+
+
+    public Speaker() {
+    }
+
+
+}
