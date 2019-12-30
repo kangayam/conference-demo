b032edc40e84e451f2b17258aab2ba122afa7558
Kangayam <bkaru1@gmail.com>
2019-12-30 13:40:45 -0600

    Heroku change - Removed hard coded DB_URL from persistance configuration file.

--- src/main/java/com/conference/conferencedemo/config/PersistenceConfiguration.java
+++ src/main/java/com/conference/conferencedemo/config/PersistenceConfiguration.java
@@ -10,15 +10,15 @@ import javax.sql.DataSource;
 // This class will override application.properties file. You build your own configuration.
 @Configuration
 public class PersistenceConfiguration {
-    @Bean
-
-    public DataSource dataSource(){
-        DataSourceBuilder builder = DataSourceBuilder.create();
-        builder.url("jdbc:postgresql://localhost:5432/conference_app");
-        builder.username("postgres");
-        builder.password("Welcome");
-        System.out.println("My custom datasource from persistanceConfiguration class");
-        return builder.build();
-
-    }
+//    @Bean
+//
+//    public DataSource dataSource(){
+//        DataSourceBuilder builder = DataSourceBuilder.create();
+//        builder.url("jdbc:postgresql://localhost:5432/conference_app");
+//        builder.username("postgres");
+//        builder.password("Welcome");
+//        System.out.println("My custom datasource from persistanceConfiguration class");
+//        return builder.build();
+//
+//    }
 }
